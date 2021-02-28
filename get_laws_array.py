4caa049daf568d72f7af8edd29b8341d03b255a4
daniel vardi <vardidan@post.bgu.ac.il>
2021-02-28 17:17:25 +0200

    added more law from lawsfile.txt

--- get_laws_array.py
+++ get_laws_array.py
@@ -6,13 +6,25 @@ if __name__ == '__main__':
     arr_of_laws = []
     file = open('C:\\Users\\CPuser\\Desktop\\LawReferral\\LawReferralGenerator\\uriMap.json', 'r', encoding='utf-8', errors='ignore')
     f = file.read()
+    file.close()
     json_file = json.loads(f)
     for element in json_file:
-        official_name = str(element["OfficialName"].split(',')[0])
+        official_name = element["OfficialName"].split(',')[0]
         official_name = official_name.replace("(", "\\(")
         official_name = official_name.replace(")", "\\)")
+        official_name = official_name.replace("-", "(-| )")
         arr_of_laws.append(official_name)
-    file.close()
+
+    file = open('C:\\Users\\CPuser\\Desktop\\LawReferral\\LawReferralGenerator\\lawsFile.txt', 'r', encoding='utf-8',
+                errors='ignore')
+    line = file.readline()
+    while line:
+        official_name = line.split(',')[0]
+        official_name = official_name.replace("(", "\\(")
+        official_name = official_name.replace(")", "\\)")
+        official_name = official_name.replace("-", "(-| )")
+        arr_of_laws.append(official_name)
+        line = file.readline()
     file = open('arr_of_laws.json', 'w')
     json.dump(arr_of_laws, file)
     file.close()
